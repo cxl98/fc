8b84f9e1fa51cf50f24fb3269e020e7b64306dae
sj <184500237@qq.com>
2020-04-10 01:11:16 +0800

    游戏副本----打机器人...

--- fc-core/src/main/java/com/easyArch/net/MessageHandler.java
+++ fc-core/src/main/java/com/easyArch/net/MessageHandler.java
@@ -1,6 +1,7 @@
 package com.easyArch.net;
 
 import com.easyArch.fight.MonsterImp;
+import com.easyArch.net.model.CODE;
 import com.easyArch.net.model.Message;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -11,6 +12,13 @@ public class MessageHandler extends SimpleChannelInboundHandler<Message> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Message msg) throws Exception {
+        int code = msg.getMsgCode();
+        Object obj = msg.getObj();
 
+        if (code == CODE.FIGHT){
+            obj = monster.getObj(obj);
+            msg.setObj(obj);
+            ctx.writeAndFlush(msg);
+        }
     }
 }
--- fc-core/src/main/java/com/easyArch/net/model/CODE.java
+++ fc-core/src/main/java/com/easyArch/net/model/CODE.java
@@ -7,10 +7,15 @@ public class CODE {
     public static final int MESSAGE = 2;
     public static final int GROUP = 3;
     public static final int FRIEND = 4;
+
     public static final int SUCCESS = 666;
+
     public static final int ERROR = 400;
 
-    public static final int FIGHT = 500;
+    public static final int FIGHT = 500;//初始化
+    public static final int WIN = 501;
+    public static final int FAIL = 502;
+    public static final int RETURN = 999;
 
 
 }
