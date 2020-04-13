cbad81b2822b194a608120e6d54cc294bf958d23
cxl <1799359457@qq.com>
2020-04-13 19:41:42 +0800

    匹配机制的实现,第一版

---
+++ fc-core/src/test/java/com/cxl/Test.java
@@ -0,0 +1,11 @@
+package com.cxl;
+
+public class Test {
+    public static void main(String[] args) {
+        float c2=1.5f;
+        int c3=5;
+        int c4=100;
+        int u= (int) Math.pow(4,c2);// 返回第一个参数的第二个参数次幂的值。
+        System.out.println(u);
+    }
+}
